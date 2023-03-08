8f66c14968d3b1c33f7347e532545e4c3405a78f
Linger <lingangliang3@gmail.com>
2023-03-08 13:57:10 +0800

    New method to monitor specific dates.

--- flightAlert.py
+++ flightAlert.py
@@ -5,78 +5,142 @@ import math
 from datetime import datetime
 
 def pushMessage(messagetitle, messagecontent, tokens):
+    sendData = {'title': messagetitle, 'content': messagecontent}
     for tok in tokens:
         sendUrl = f'https://sctapi.ftqq.com/{tok}.send'
-        sendData = {'title': messagetitle, 'content': messagecontent}
-        # print(messageTitle)
-        # print(messageContent)
         requests.post(sendUrl, data=sendData)
 
-def processData(results):
-    result_goto, result_back = results
-    price_goto_dict = {}
-    price_back_dict = {}
-    for i_date in result_goto:
-        day = datetime.strptime(i_date, "%Y%m%d")
-        day_delta = day - datetime.today()
-        if day_delta.days >= 60:
+def printMessage(messagetitle, messagecontent, *skip):
+    print(messagetitle)
+    print(messagecontent)
+
+# Calculate the price difference between 2 days.
+def monitorCertainDates(results, priceStep, datesTogo, datesBack, history, cities):
+    resLeav, resBack = results
+    cfrom, cto = cities
+    histMinLeav = history['minLeav']
+    histLeav = history['prevLeav']
+    histMinBack = history['minBack']
+    histBack = history['prevBack']
+    toSend = False
+    resLeavDates = {}
+    resBackDates = {}
+    for date in datesTogo:
+        resLeavDates[date] = resLeav[date]
+    for date in datesBack:
+        resBackDates[date] = resBack[date]
+
+    minLeavPrice = min(resLeavDates.values())
+    if abs(minLeavPrice - histLeav) >= priceStep:
+        history['prevLeav'] = minLeavPrice
+        toSend = True
+
+    minBackPrice = min(resBackDates.values())
+    if abs(minBackPrice - histBack) >= priceStep:
+        history['prevBack'] = minBackPrice
+        toSend = True
+
+    histMinLeav = min(histMinLeav, minLeavPrice)
+    histMinBack = min(histMinBack, minBackPrice)
+    history['minLeav'] = histMinLeav
+    history['minBack'] = histMinBack
+
+    title = f' Current price to {cto}: {minLeavPrice} ↔ {minBackPrice}'
+    content = f'*{cfrom}–{cto}:*\n\n'
+    for date, price in resLeavDates.items():
+        content += '**' + date[4:6] + '-' + date[6:8] + f': {price}**\n\n'
+    content += f'History lowest: {histMinLeav}\n\n'
+    content += '---\n\n'
+    content += f'*{cto}–{cfrom}:*\n\n'
+    for date, price in resBackDates.items():
+        content += '**' + date[4:6] + '-' + date[6:8] + f': {price}**\n\n'
+    content += f'History lowest: {histMinBack}'
+
+    return title, content, toSend
+
+# Obtain the lowest price for departure and return trip.
+def monitor2MonthWeekend(results, targetPrice, **skip):
+    resLeav, resBack = results
+    priceLeavDict = {}
+    priceBackDict = {}
+    for date in resLeav:
+        day = datetime.strptime(date, "%Y%m%d")
+        deltaDay = day - datetime.today()
+        if deltaDay.days >= 60:
             break
-        day_weekday = day.weekday() + 1
-        if day_weekday >= 4: # skip Monday to Wednesday
-            price_goto_dict[day.strftime("%m-%d") + r"(" + str(day_weekday) + r")"] = result_goto[i_date]
-            price_back_dict[day.strftime("%m-%d") + r"(" + str(day_weekday) + r")"] = result_back[i_date]
 
-    min_goto_price = min(price_goto_dict.values())
-    dateWithMin = [x for x in price_goto_dict if price_goto_dict[x] == min_goto_price]
+        weekday = day.weekday() + 1
+        if weekday >= 4: # skip Monday to Wednesday
+            priceLeavDict[day.strftime("%m-%d") + r"(" + str(weekday) + r")"] = resLeav[date]
+            priceBackDict[day.strftime("%m-%d") + r"(" + str(weekday) + r")"] = resBack[date]
 
-    title = f'当前低价: {min_goto_price} ' + dateWithMin[0]
-    content = "### " + " ".join(dateWithMin)
-    content += "\n\n所有日期价格:\n\n&emsp;&emsp;&emsp;&emsp;SHA–BJS&ensp;BJS–SHA\n\n---"
-    for i_date in price_goto_dict:
+    minLeavPrice = min(priceLeavDict.values())
+    dateMinLeav = [x for x in priceLeavDict if priceLeavDict[x] == minLeavPrice]
+    minBackPrice = min(priceBackDict.values())
+    dateMinBack = [x for x in priceBackDict if priceBackDict[x] == minBackPrice]
+    maxMinPrice = max(minLeavPrice, minBackPrice)
+
+    title = f' Lowest price: {minLeavPrice} (SHA–BJS) ↔ {minBackPrice} (BJS-SHA)'
+    content = f"### {minLeavPrice} (SHA–BJS): " + " ".join(dateMinLeav)
+    content += f"\n\n### {minBackPrice} (BJS-SHA): " + " ".join(dateMinBack)
+    content += "\n\nPrice list:\n\n&emsp;&emsp;&emsp;&emsp;SHA–BJS&ensp;BJS–SHA\n\n---"
+    for date in priceLeavDict:
         content += "\n\n"
-        day_weekday = i_date[6]
-        content += i_date + ":&emsp;" \
-            + ("**" if price_goto_dict[i_date] <= min_goto_price else "") \
-            + str(price_goto_dict[i_date]) \
-            + ("**" if price_goto_dict[i_date] <= min_goto_price else "") \
+        weekday = date[6]
+        content += date + ":&emsp;" \
+            + ("**" if priceLeavDict[date] <= maxMinPrice else "") \
+            + str(priceLeavDict[date]) \
+            + ("**" if priceLeavDict[date] <= maxMinPrice else "") \
             + "&emsp;&emsp;&emsp;" \
-            + ("**" if price_back_dict[i_date] <= min_goto_price else "") \
-            + str(price_back_dict[i_date]) \
-            + ("**" if price_back_dict[i_date] <= min_goto_price else "")
-        if day_weekday == "7":
+            + ("**" if priceBackDict[date] <= maxMinPrice else "") \
+            + str(priceBackDict[date]) \
+            + ("**" if priceBackDict[date] <= maxMinPrice else "")
+        if weekday == "7":
             content += "\n\n---"
 
-    return title, content, min_goto_price
+    toSend = maxMinPrice <= targetPrice
+    return title, content, toSend
 
 if __name__ == "__main__":
+    test = False
+    mode = 1
+
+    baseUrl = "https://flights.ctrip.com/itinerary/api/12808/lowestPrice?"
     # first import the json config file
     with open("config.json") as f:
         config = json.load(f)
     # assemble the request url
-    baseUrl = "https://flights.ctrip.com/itinerary/api/12808/lowestPrice?"
-    url_goto = f'https://flights.ctrip.com/itinerary/api/12808/lowestPrice?flightWay={config["flightWay"]}&dcity={config["placeFrom"]}&acity={config["placeTo"]}&direct=true&army=false'
-    url_back = f'https://flights.ctrip.com/itinerary/api/12808/lowestPrice?flightWay={config["flightWay"]}&dcity={config["placeTo"]}&acity={config["placeFrom"]}&direct=true&army=false'
+    urlLeav = f'https://flights.ctrip.com/itinerary/api/12808/lowestPrice?flightWay={config["flightWay"]}&dcity={config["placeFrom"]}&acity={config["placeTo"]}&direct=true&army=false'
+    urlBack = f'https://flights.ctrip.com/itinerary/api/12808/lowestPrice?flightWay={config["flightWay"]}&dcity={config["placeTo"]}&acity={config["placeFrom"]}&direct=true&army=false'
 
     # get the price periodicly and alert through wechat
     targetPrice = config["targetPrice"]
-    # histMin = 9999 # renew each month
-    is_pm = True
+    priceStep = config["priceStep"]
+    history = {"minLeav": 9999, "prevLeav": 0, "minBack": 9999, "prevBack": 0}
+    releaseMessage = printMessage if test else pushMessage
     while True:
-        r_goto = requests.get(url_goto)
-        r_back = requests.get(url_back)
-        if r_goto.status_code != 200 or r_goto.json()["status"] == 2 or r_back.status_code != 200 or r_back.json()["status"] == 2:
-            pushMessage('机票信息获取失败', "### " + f'{time.strftime("%Y-%m-%d %H:%M")}', config["ftqq_SCKEY"])
+        # request the url
+        reqLeav = requests.get(urlLeav)
+        reqBack = requests.get(urlBack)
+
+        if reqLeav.status_code != 200 or reqLeav.json()["status"] == 2 or reqBack.status_code != 200 or reqBack.json()["status"] == 2:
+            releaseMessage('Failed to retrieve flight ticket information.', "### " + f'{time.strftime("%Y-%m-%d %H:%M")}', config["ftqq_SCKEY"])
         else:
-            result_goto = r_goto.json()["data"]["oneWayPrice"][0]
-            result_back = r_back.json()["data"]["oneWayPrice"][0]
-            messageTitle, messageContent, minPrice = processData([result_goto, result_back])
-            # histMin = min(minPrice, histMin)
-            if minPrice <= 490:
-                pushMessage(messageTitle, messageContent, config["ftqq_SCKEY"])
-        print(f'{time.strftime("%Y-%m-%d %H:%M")}: 查询完毕')
-        if is_pm:
-            time.sleep((datetime.strptime("11:00:00", "%H:%M:%S") - datetime.today()).seconds)
-            is_pm = False
+            # analyse the results
+            resLeav = reqLeav.json()["data"]["oneWayPrice"][0]
+            resBack = reqBack.json()["data"]["oneWayPrice"][0]
+            if mode == 1:
+                messageTitle, messageContent, toSend = monitorCertainDates(
+                    [resLeav, resBack], priceStep, config['dateToGo'], config['dateBack'], history, [config['placeFrom'], config['placeTo']])
+            elif mode == 2:
+                messageTitle, messageContent, toSend = monitor2MonthWeekend([resLeav, resBack], targetPrice=targetPrice)
+            # send message
+            if toSend:
+                releaseMessage(messageTitle, messageContent, config["ftqq_SCKEY"])
+
+        print(f'{time.strftime("%Y-%m-%d %H:%M")} finished. Waiting for tomorrow.')
+
+        if not test:
+            time.sleep((datetime.strptime("17:00:00", "%H:%M:%S") - datetime.now()).seconds)
         else:
-            time.sleep((datetime.strptime("17:00:00", "%H:%M:%S") - datetime.today()).seconds)
-            is_pm = True
+            break
